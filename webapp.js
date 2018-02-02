2ea5ec974a408894f5f3f98cff724a0a042aacb2
Ansh Kakkar <Ansh.kakkar@warriorlife.net>
2018-02-02 10:37:50 -0800

    fixed buttons

--- webapp.js
+++ webapp.js
@@ -11,9 +11,12 @@ window.onload = function() {
 
 function start_game() {
 	// hide the start button
-	document.getElementById("startBtn").style.visibility = "hidden";
+	document.getElementById("startBtn").style.display = "none";
 	// show the buttons
-	
+		// button1
+			document.getElementById("fight").style.display = "inline";
+			// button2
+			document.getElementById("run").style.display = "inline";
 	// load the first scene
 	updateScene("s1");
 }
@@ -22,10 +25,7 @@ function updateScene(s) {
 	switch(s) {
 		case "s1":
 			document.getElementById("story").innerHTML = "<p>You live in a small village in the forest.</p><p>After walking through the wood, you find a deer drinking water.</p>";
-			// button1
-			document.getElementById("fight").style.visibility = "visible";
-			document.getElementById("run").style.visibility = "visible";
-			// button2
+			
 			break;
 		case "s2":
 			break;
